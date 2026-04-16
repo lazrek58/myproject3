0f7713560787530d96f78c19f2b8285ceb2a66fb
lazrek58 <laz.abdelhamid58@gmail.com>
2026-04-16 08:01:59 +0100

    Add files via upload

---
+++ graph2.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# إنشاء القيم
+x = np.linspace(0, 10, 100)
+y = x**2
+
+# رسم المنحنى
+plt.plot(x, y)
+
+# عنوان
+plt.title("$y = x^2$")
+
+# حفظ الصورة
+plt.savefig("/storage/emulated/0/Download/graphe1.png")
