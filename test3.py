21cc4381b50ce6633625f70a3f30951a7544bc4a
lazrek58 <laz.abdelhamid58@gmail.com>
2026-04-16 08:18:34 +0100

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,15 @@
+import import matplotlib.pyplot as plt
+import numpy as np
+
+theta = np.linspace(0, 2*np.pi, 100)
+x = np.cos(theta)
+y = np.sin(theta)
+plt.plot(x,y)
+plt.axis('equal')  # هاد السطر مهم باش الدائرة تبان صحيحة
+plt.xlabel("L'axe X")
+plt.ylabel("L'axe Y")
+plt.savefig("/storage/emulated/0/Download/test4.png")
+plt.grid(True)
+
+
+
